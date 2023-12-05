e22a1e20db08863fb771f42c8abc5cd89d9894c1
Ted Zhao <zp4work+dev@gmail.com>
2023-12-05 19:08:17 +1030

    Add Dagster project from `dagster-dbt project scaffold`

---
+++ dbt_cloud_demo/dbt_cloud_demo/definitions.py
@@ -0,0 +1,17 @@
+import os
+
+from dagster import Definitions
+from dagster_dbt import DbtCliResource
+
+from .assets import my_new_project_dbt_assets
+from .constants import dbt_project_dir
+from .schedules import schedules
+
+defs = Definitions(
+    assets=[my_new_project_dbt_assets],
+    schedules=schedules,
+    resources={
+        "dbt": DbtCliResource(project_dir=os.fspath(dbt_project_dir)),
+    },
+)
+
